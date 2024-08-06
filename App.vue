8ec79742f0a4e5b1803ce6069edb5f3ade52a8da
mbayedeweb <ibzembaye@gmail.com>
2024-08-06 02:31:46 +0000

    templaye

---
+++ App.vue
@@ -0,0 +1,35 @@
+<script setup></script>
+
+<template>
+  <h1>Liste des Produits</h1>
+  <p>E-Commerce App</p>
+  <h1>Nos Produits</h1>
+  <p>Iphone</p>
+  <h1>AUTRES</h1>
+  <p>Samsung</p>
+  <p>TV</p>
+  <p>Motorola</p>
+  <p>Ordinateur</p>
+  <form>
+    <label> Form </label>
+    <input>
+    <button>Ajouter un Nouveau produit</button>
+  </form>
+
+</template>
+
+<style scoped>
+h1{
+color: rgb(6, 104, 104);
+}
+p{
+  font-size: bold;
+}
+form{
+  background-color: yellowgreen;
+}
+</style>
+
+
+
+
---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script setup></script>
+
+<template>
+  <h1>Liste des Produits</h1>
+  <p>E-Commerce App</p>
+  <h1>Nos Produits</h1>
+  <p>Iphone</p>
+  <h1>AUTRES</h1>
+  <p>Samsung</p>
+  <p>TV</p>
+  <p>Motorola</p>
+  <p>Ordinateur</p>
+  <form>
+    <label> Form </label>
+    <input>
+    <button>Ajouter un Nouveau produit</button>
+  </form>
+
+</template>
+
+<script>
+// export default {
+//   name: 'HelloWorld',
+//   props: {
+//     msg: String
+//   }
+// }
+</script>
+
+<style scoped>
+h1{
+color: rgb(6, 104, 104);
+}
+p{
+  font-size: bold;
+}
+form{
+  background-color: green;
+}
+</style>
+
+
+
+
